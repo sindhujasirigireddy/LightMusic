24931ba23340c5f15f0239a2cfe4b78495dc27db
Sindhuja Sirigireddy <Sindhuja.Sirigireddy@ggktech.local>
2016-04-12 16:15:31 +0530

    Initial commit

---
+++ app/src/main/java/com/sindhujasirigireddy/lightmusic/Song.java
@@ -0,0 +1,29 @@
+package com.sindhujasirigireddy.lightmusic;
+
+/**
+ * Created by sindhuja.sirigireddy on 4/12/2016.
+ */
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+
+}
